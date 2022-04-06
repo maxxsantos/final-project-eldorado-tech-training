4aa015c825d8496a8229a9d6f4577b4423bb2c13
Max <max.santos@icomp.ufam.edu.br>
2022-04-06 18:34:40 -0400

    fix: put entities in entities environmente config

--- back-end/src/api_db/configs/database.ts
+++ back-end/src/api_db/configs/database.ts
@@ -1,13 +1,17 @@
+import { Device } from './../entities/Device';
+import { Category } from './../entities/Category';
 import { DataSourceOptions } from "typeorm";
 
 export const config: DataSourceOptions = {
 	type: "mysql",
-	host: "localhost",
-	port: 3336,
+	host: "api-db",
+	port: 3306,
 	username: "test",
 	password: "test",
 	database: "test",
-	entities: [],
 	logging: true,
 	synchronize: true,
+	entities: [Category, Device],
+	migrations: ['../migrations/**/*.ts'],
+	
 }
